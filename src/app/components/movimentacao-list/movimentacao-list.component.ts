a195c02729c9345b2041008ae352d4ed8cf0e934
larissadiasdesena <larisenna17@gmail.com>
2022-05-05 12:39:23 -0300

    Atualizado

---
+++ src/app/components/movimentacao-list/movimentacao-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movimentacao-list',
+  templateUrl: './movimentacao-list.component.html',
+  styleUrls: ['./movimentacao-list.component.css']
+})
+export class MovimentacaoListComponent implements OnInit {
+
+  movimentacoes:any;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
