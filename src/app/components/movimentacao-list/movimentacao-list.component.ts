6c2bb1c791bce90d232b0daace736fc4ccf8aa5e
larissadiasdesena <larisenna17@gmail.com>
2022-05-05 22:29:49 -0300

    update

--- src/app/components/movimentacao-list/movimentacao-list.component.ts
+++ src/app/components/movimentacao-list/movimentacao-list.component.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MovimentacaoService } from 'src/app/services/movimentacao.service';
 @Component({
   selector: 'app-movimentacao-list',
   templateUrl: './movimentacao-list.component.html',
   styleUrls: ['./movimentacao-list.component.css']
 })
 export class MovimentacaoListComponent implements OnInit {
-
   movimentacoes:any;
+
   
-  constructor() { }
+  constructor(private movimentacaoService: MovimentacaoService) { }
 
   ngOnInit(): void {
+    
   }
+  listMovimentacoes(): void {
+    this.movimentacaoService.list()
+      .subscribe(
+        data => {
+          this.movimentacoes = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
 
 }
